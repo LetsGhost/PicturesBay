594778d580d63ccaa1d47a0cea0004875f2828d9
Paul Ehrling <ghostlest330@gmail.com>
2024-08-20 15:41:28 +0200

    chore: Update app.ts to include passport and session middleware

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import passport from 'passport';
+import session from 'express-session';;
 dotenv.config();
 
 import {connectToDatabase} from "./configs/db";
+import './configs/passportConfig';
 
 import userRouter from './app/routes/user.routes';
 
@@ -22,8 +25,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Passport
+app.use(session({ 
+  secret: process.env.SECRET_KEY as string, 
+  resave: false, 
+  saveUninitialized: true 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
-app.use(userRouter)
+app.use("/user", userRouter)
 
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
