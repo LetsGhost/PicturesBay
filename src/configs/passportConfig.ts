b479576e693efb639a1256fdbab659ab4e69ee07
Paul Ehrling <ghostlest330@gmail.com>
2024-08-20 15:41:42 +0200

    chore: Add authentication middleware for route protection

--- src/configs/passportConfig.ts
+++ src/configs/passportConfig.ts
@@ -10,15 +10,17 @@ passport.use(new LocalStrategy(
   },
   async (email, password, done) => {
     try {
-      const user = await userService.findUserByEmail(email);
-      if (!user) {
+      const {success, code, user} = await userService.findUserByEmail(email);
+      if (!success) {
         return done(null, false, { message: 'Incorrect email.' });
       }
-      const isValid = await userService.validatePassword(password, user.password);
-      if (!isValid) {
+      console.log('User found:', user);
+
+      const isValid = await userService.validatePassword(password, user?.password ?? "");
+      if (!isValid.success) {
         return done(null, false, { message: 'Incorrect password.' });
       }
-      return done(null, user);
+      return done(null, user as any);
     } catch (err) {
       return done(err);
     }
@@ -26,12 +28,12 @@ passport.use(new LocalStrategy(
 ));
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, (user as User)._id);
 });
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await userService.findUserById(id);
+    const user = await userService.findUserById(id as string);
     done(null, user);
   } catch (err) {
     done(err, null);
