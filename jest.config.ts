b3b148c696396203147df7122f0a0527f0984d8e
Paul Ehrling <ghostlest330@gmail.com>
2024-09-04 18:40:08 +0200

    chore: Add jest.config.ts for configuring Jest test runner

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+};
+
+export default config;
