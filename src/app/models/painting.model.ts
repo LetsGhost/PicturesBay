5181b4f8e15409681869d5045afb5ac95f59c781
Paul Ehrling <ghostlest330@gmail.com>
2024-09-16 09:45:07 +0200

    feat: Add condition field to Painting model and set default value
    Add a new field called "condition" to the Painting model in order to track the condition of each painting. The field is required and has a default value of 100. This change ensures that the condition of each painting is properly recorded and can be used for further analysis or filtering.

--- src/app/models/painting.model.ts
+++ src/app/models/painting.model.ts
@@ -2,13 +2,14 @@ import { prop } from '@typegoose/typegoose';
 import { Document } from 'mongoose';
 
 class Painting extends Document {
-  constructor(name: string, level: string, painting: string, createdAt: Date, number: number) {
+  constructor(name: string, level: string, painting: string, createdAt: Date, number: number, condition: number) {
     super();
     this.name = name;
     this.level = level;
     this.painting = painting;
     this.number = number;
     this.createdAt = createdAt;
+    this.condition = condition
   }
 
   @prop({ required: true })
@@ -22,6 +23,9 @@ class Painting extends Document {
 
   @prop({ required: true, })
   public number: number;
+
+  @prop({ required: true })
+  public condition: number
 
   @prop({ required: true, default: Date.now })
   public createdAt: Date;
