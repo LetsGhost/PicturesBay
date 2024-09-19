03186abf2677747eed74ae72844aa00ea7d4e2f6
Paul Ehrling <ghostlest330@gmail.com>
2024-09-19 14:31:13 +0200

    refactor: Update Room interface in Room.types.ts to include paintingBets

--- src/app/types/Room.types.ts
+++ src/app/types/Room.types.ts
@@ -1,3 +1,17 @@
+import { PaintingInterface } from './Painting.types';
+
+export interface PaintingBet{
+  paintingId: string;
+  paintingName: string;
+  lastBet: number;
+  currentBet: number;
+  currentBetUser: string;
+  winner: {
+    userId: string;
+    username: string;
+  }
+}
+
 export interface Room{
   name: string;
   timer: NodeJS.Timeout;
@@ -5,4 +19,6 @@ export interface Room{
   users: Set<string>;
   oneMinuteInterval: NodeJS.Timeout;
   creationTime: number;
+  paintings: PaintingInterface[];
+  paintingBets: PaintingBet[];
 }
