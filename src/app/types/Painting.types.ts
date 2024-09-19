316e2cc9b3f05c4a37da47b65f560085d7b31e84
Paul Ehrling <ghostlest330@gmail.com>
2024-09-19 14:31:09 +0200

    refactor: Update Painting interface in Painting.types.ts

--- src/app/types/Painting.types.ts
+++ src/app/types/Painting.types.ts
@@ -1,4 +1,7 @@
-export interface Painting {
+import Painting from "../models/painting.model";
+
+export interface PaintingInterface {
+  _id: Painting['_id'];
   name: string;
   level: string;
   painting: string;
